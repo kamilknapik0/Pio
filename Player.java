d4ecb05c0afaa43ded65650523a8924ecc831606
kamil <147108855+kamilknapik0@users.noreply.github.com>
2023-10-22 14:52:31 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,20 +1,27 @@
 import java.util.Random;
 
 public class Player {
+
     private Random brain = new Random();
+
+    private String name = "User";
+
+    public Player() {} //przeciazenie
+
+    public Player(String name){ //konstruktor
+        setName(name);
+    }
 
     public int guess() {
         return brain.nextInt(6) + 1;
     }
-
-    private String name;
 
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        if (name != null) {
+        if (name != null && !name.isEmpty()) {
             this.name = name;
         } else {
             System.out.println("Nieprawidłowe imie");
